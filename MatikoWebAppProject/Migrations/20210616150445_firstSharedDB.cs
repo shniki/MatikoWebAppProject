feb1e1f0bf17e37f3f58e1d5d5469146b495cc3f
Yaniv Hacker <yanivhackerr@gmail.com>
2021-06-16 18:06:39 +0300

    opened and created our shared database

---
+++ MatikoWebAppProject/Migrations/20210616150445_firstSharedDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MatikoWebAppProject.Migrations
+{
+    public partial class firstSharedDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
